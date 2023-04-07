9632362d3e4202b5d873d9720c8c2f4cf9b9f069
Stas Soloviov <stas0601019@gmail.com>
2023-04-07 15:16:58 +0300

    models

---
+++ passport_office/models.py
@@ -0,0 +1,77 @@
+from passport_office import db
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primery_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    middle_name = db.Column(db.String(128))
+    date_of_birth = db.Column(db.String(50), nullable=False)
+    sex = db.Column(db.String(30), nullable=False)
+
+    marriage = db.relationship('Marriage', backref='person')
+    death = db.relationship('Death', backref='person')
+    sex_change = db.relationship('SexChange', backref='person')
+    birth = db.relationship('Birth', backref='person')
+    adoption = db.relationship('Adoption', backref='person')
+    history = db.relationship('History', backref='person')
+    genealogy = db.relationship('Genealogy', backref='person')
+
+
+class Marriage:
+    id = db.Column(db.Integer, primery_key=True)
+    husband_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    wife_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    date_of_marriage = db.Column(db.DateTime, nullable=False)
+
+    divorce = db.relationship('Divorce', backref='marriage')
+
+
+class Divorce:
+    id = db.Column(db.Integer, primery_key=True)
+    marriage_id = db.Column(db.Integer, db.ForeignKey('marriage.id'), nullable=False)
+    date_of_divorce = db.Column(db.DateTime, nullable=False)
+
+
+class Death:
+    id = db.Column(db.Integer, primery_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    date_of_death = db.Column(db.DateTime, nullable=False)
+
+
+class SexChange:
+    id = db.Column(db.Integer, primery_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    date_of_change = db.Column(db.DateTime, nullable=False)
+    new_sex = db.Column(db.VARCHAR(30), nullable=False)
+
+
+class Birth:
+    id = db.Column(db.Integer, primery_key=True)
+    father_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    mother_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    child_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    date_of_birth = db.Column(db.DateTime, nullable=False)
+
+
+class Adoption:
+    id = db.Column(db.Integer, primery_key=True)
+    adoptive_father_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    adoptive_mother_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    adopted_child_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    date_of_adopt = db.Column(db.DateTime, nullable=False)
+
+
+class History:
+    id = db.Column(db.Integer, primery_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    date_of_change = db.Column(db.DateTime, nullable=False)
+    changed_parameter = db.Column(db.VARCHAR(100), nullable=False)
+    changed_value = db.Column(db.VARCHAR(100), nullable=False)
+
+
+class Genealogy:
+    id = db.Column(db.Integer, primery_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    generation = db.Column(db.Integer, nullable=False)
