e90e674a1dc6b6698d5509e298623224d24e6581
Stas Soloviov <stas0601019@gmail.com>
2023-04-13 14:25:44 +0300

    fix:models, add:interface functions

--- passport_office/interface.py
+++ passport_office/interface.py
@@ -1,54 +1,81 @@
+from sqlalchemy.exc import DatabaseError
+
 from passport_office import db
 from passport_office.models import Person, Marriage, Divorce
 
 
 def add_person(name, last_name, middle_name, date_of_birth, sex):
+    try:
+        # person = Person(name, last_name, middle_name, date_of_birth, sex)
+        person = Person(
+            name=name,
+            last_name=last_name,
+            middle_name=middle_name,
+            date_of_birth=date_of_birth,
+            sex=sex
+        )
 
-    person = Person(
-        name=name,
-        last_name=last_name,
-        middle_name=middle_name,
-        date_of_birth=date_of_birth,
-        sex=sex
-    )
+        if not Person:  # ( if not kwargs in Person) -  перевірка на наявність прийнятих обов'язкових аргументів(доробить)
+            raise Exception
 
-    db.session.add(person)
-    db.session.commit()
+        db.session.add(person)
+        db.session.commit()
+
+    except DatabaseError:
+        db.session.rollback()
 
 
-def marriage_status(husband_id, wife_id, date_of_marriage):
+def marriage_add(husband_id, wife_id, date_of_marriage):
+    try:
+        marriage = Marriage(
+            husband_id=husband_id,
+            wife_id=wife_id,
+            date_of_marriage=date_of_marriage
+        )
 
-    marriage = Marriage(
-        husband_id=husband_id,
-        wife_id=wife_id,
-        date_of_marriage=date_of_marriage
-    )
+        db.session.add(marriage)
+        db.session.commit()
 
-    db.session.add(marriage)
-    db.session.commit()
+    except DatabaseError:
+        db.session.rollback()
 
 
 # def marriage_add(hus_id, wif_id, date_of_marriage):
+#     try:
+#         husband_id = (Person.query.filter_by(id=hus_id).first()).id
+#         wife_id = (Person.query.filter_by(id=wif_id).first()).id
 #
-#     husband_id = (Person.query.filter_by(id=hus_id).first()).id
-#     wife_id = (Person.query.filter_by(id=wif_id).first()).id
+#         marriage = Marriage(
+#             husband_id=husband_id,
+#             wife_id=wife_id,
+#             date_of_marriage=date_of_marriage
+#         )
 #
-#     marriage = Marriage(
-#         husband_id=husband_id,
-#         wife_id=wife_id,
-#         date_of_marriage=date_of_marriage
-#     )
+#         db.session.add(marriage)
+#         db.session.commit()
 #
-#     db.session.add(marriage)
-#     db.session.commit()
+#     except DatabaseError:
+#         db.session.rollback()
 
 
 def divorce_add(marriage_id, date_of_divorce):
+    try:
+        # add divorce
+        divorce = Divorce(
+            marriage_id=marriage_id,
+            date_of_divorce=date_of_divorce
+        )
+        db.session.add(divorce)
 
-    divorce = Divorce(
-        marriage_id=marriage_id,
-        date_of_divorce=date_of_divorce
-    )
+        # annulment of marriage
+        marriage = Marriage.query.filter_by(id=marriage_id).first()
+        if not marriage:  # перевірка на наявність marriage в db
+            raise Exception
 
-    db.session.add(divorce)
-    db.session.commit()
+        db.session.delete(marriage)
+        db.session.commit()
+
+    except DatabaseError:
+        db.session.rollback()
+
+
--- passport_office/models.py
+++ passport_office/models.py
@@ -2,6 +2,7 @@ from passport_office import db
 
 
 class Person(db.Model):
+    __tablename__ = "person"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
@@ -10,26 +11,38 @@ class Person(db.Model):
     date_of_birth = db.Column(db.String(50), nullable=False)
     sex = db.Column(db.String(30), nullable=False)
 
-    marriage = db.relationship('Marriage', backref='person')
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.name = kwargs.get('last_name')
+        self.name = kwargs.get('middle_name')
+        self.name = kwargs.get('date_of_birth')
+        self.name = kwargs.get('sex')
+
+    # marriage = db.relationship('Marriage', lazy=True)
     death = db.relationship('Death', backref='person')
     sex_change = db.relationship('SexChange', backref='person')
-    birth = db.relationship('Birth', backref='person')
-    adoption = db.relationship('Adoption', backref='person')
+    # birth = db.relationship('Birth', backref='person')
+    # adoption = db.relationship('Adoption', backref='person')
     history = db.relationship('History', backref='person')
-    genealogy = db.relationship('Genealogy', backref='person')
+    # genealogy = db.relationship('Genealogy', backref='person')
 
 
 class Marriage(db.Model):
+    __tablename__ = "marriage"
 
     id = db.Column(db.Integer, primary_key=True)
     husband_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     wife_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     date_of_marriage = db.Column(db.DateTime, nullable=False)
 
+    husband = db.relationship('Person', foreign_keys="Marriage.husband_id")
+    wife = db.relationship('Person', foreign_keys="Marriage.wife_id")
+
     divorce = db.relationship('Divorce', backref='marriage')
 
 
 class Divorce(db.Model):
+    __tablename__ = "divorce"
 
     id = db.Column(db.Integer, primary_key=True)
     marriage_id = db.Column(db.Integer, db.ForeignKey('marriage.id'), nullable=False)
@@ -37,6 +50,7 @@ class Divorce(db.Model):
 
 
 class Death(db.Model):
+    __tablename__ = "death"
 
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
@@ -52,6 +66,7 @@ class SexChange(db.Model):
 
 
 class Birth(db.Model):
+    __tablename__ = "birth"
 
     id = db.Column(db.Integer, primary_key=True)
     father_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
@@ -59,8 +74,13 @@ class Birth(db.Model):
     child_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
     date_of_birth = db.Column(db.DateTime, nullable=False)
 
+    father = db.relationship('Person', foreign_keys="Birth.father_id")
+    mother = db.relationship('Person', foreign_keys="Birth.mother_id")
+    child = db.relationship('Person', foreign_keys="Birth.child_id")
+
 
 class Adoption(db.Model):
+    __tablename__ = "adoption"
 
     id = db.Column(db.Integer, primary_key=True)
     adoptive_father_id = db.Column(db.Integer, db.ForeignKey('person.id'))
@@ -68,19 +88,28 @@ class Adoption(db.Model):
     adopted_child_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     date_of_adopt = db.Column(db.DateTime, nullable=False)
 
+    adoptive_father = db.relationship('Person', foreign_keys="Adoption.adoptive_father_id")
+    adoptive_mother = db.relationship('Person', foreign_keys="Adoption.adoptive_mother_id")
+    adopted_child = db.relationship('Person', foreign_keys="Adoption.adopted_child_id")
+
 
 class History(db.Model):
+    __tablename__ = "history"
 
     id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
     date_of_change = db.Column(db.DateTime, nullable=False)
-    changed_parameter = db.Column(db.VARCHAR(100), nullable=False)
-    changed_value = db.Column(db.VARCHAR(100), nullable=False)
+    changed_parameter = db.Column(db.String(100), nullable=False)
+    changed_value = db.Column(db.String(100), nullable=False)
 
 
 class Genealogy(db.Model):
+    __tablename__ = "genealogy"
 
     id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    parent_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    parent_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     generation = db.Column(db.Integer, nullable=False)
+
+    person = db.relationship('Person', foreign_keys="Genealogy.person_id")
+    parent = db.relationship('Person', foreign_keys="Genealogy.parent_id")
